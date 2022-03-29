49623b8deec31631f96d60dcebbb212a55e864a1
David Sarrat González <git@davidsarratgonzalez.com>
2022-03-30 01:41:50 +0200

    Fixed webdriver timeout error

--- desaparecidos/common/driver.py
+++ desaparecidos/common/driver.py
@@ -2,10 +2,17 @@ from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.options import Options
 
+USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) " \
+             "AppleWebKit/537.36 (KHTML, like Gecko) " \
+             "Chrome/87.0.4280.88 Safari/537.36"
+
 
 def set_driver():
     options = Options()
     options.add_argument('--headless')
+    options.add_argument('--disable-gpu')
+    options.add_argument("user-agent=" + USER_AGENT)
+
     driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
     return driver
 
