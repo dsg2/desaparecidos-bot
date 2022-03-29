d3e2f575c1bf3c1cef6faa794203015db14d0e28
David Sarrat González <git@davidsarratgonzalez.com>
2022-03-30 01:26:41 +0200

    Added headless mode

--- desaparecidos/common/driver.py
+++ desaparecidos/common/driver.py
@@ -1,9 +1,12 @@
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.options import Options
 
 
 def set_driver():
-    driver = webdriver.Chrome(ChromeDriverManager().install())
+    options = Options()
+    options.add_argument('--headless')
+    driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
     return driver
 
 
